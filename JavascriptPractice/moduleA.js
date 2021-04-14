5cd707c13b38ad1fa8fbf7b9f9ebd61953f9668a
Cameron Stoner <Cameron.Stoner@orion.com>
2021-04-14 08:21:59 -0500

    modules dependent on other modules I created

--- JavascriptPractice/moduleA.js
+++ JavascriptPractice/moduleA.js
@@ -1,9 +1,11 @@
 
 import * as Circle from './modules/circle.js';
 import * as Canvas from './modules/canvas.js';
-
+import * as ModB from './moduleB.js';
 
 export function drawPinkCanvas(id, parent, width, height){
     let myCanvas = Canvas.create(id, parent, width, height);
     let circle1 = Circle.draw(myCanvas.ctx, 175, 200, 300, 'pink');
+
+    ModB.alertMe('Look I drew pink!');
 }
---
+++ JavascriptPractice/moduleB.js
@@ -0,0 +1,3 @@
+export function alertMe(message){
+    alert(message);
+}
