8c572183c2f6c6acfd85302385e04a859743a9d3
Cameron Stoner <Cameron.Stoner@orion.com>
2021-05-06 21:11:57 -0500

    finished router nav tutorial

--- AngularRouterSample/angular-router-sample/src/app/app.module.ts
+++ AngularRouterSample/angular-router-sample/src/app/app.module.ts
@@ -5,18 +5,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'crisis-list', component: CrisisListComponent },
       { path: 'heroes-list', component: HeroesListComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
     ]),
   ],
   providers: [],
